7609ea0c547fbd7b61a1645c8c0d4912992de8ec
victor DALET <victor.dalet@protonmail.com>
2021-03-24 22:33:45 +0100

    commentaire

--- web/projet/js/message.js
+++ web/projet/js/message.js
@@ -1,8 +1,8 @@
-function envoieTouche()
+function envoieTouche() // fonction qui permet de tdetecter si une touche est frappé
 {
-    var event = window.event || touche;
-     if (event.keyCode == 13 ) // entrer
+    var event = window.event || touche; // constante d'évenement
+     if (event.keyCode == 13 ) // s'il s'agit de la touche entrer
      {
-     	document.getElementById("envoie"). submit();
+     	document.getElementById("envoie"). submit(); /*on envoie le message*/
      }
 }
