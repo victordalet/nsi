68b407928596f983943e5997a7393cd3b2114815
victor DALET <victor.dalet@protonmail.com>
2021-05-02 21:40:32 +0200

    mini oublis

--- python/tp liste/display.py
+++ python/tp liste/display.py
@@ -1,4 +1,4 @@
 def display(nouvelle_liste,resultat,prenoms_max):
-    print("liste sans doublons".format(nouvelle_liste))
-    print("doublons : ".format(resultat))
-    print("le prenom le plus présent est ".format(prenoms_max))
+    print("liste sans doublons {}".format(nouvelle_liste))
+    print("doublons : {}".format(resultat))
+    print("le prenom le plus présent est {}".format(prenoms_max))
